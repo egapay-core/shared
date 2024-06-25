996335472afa746e14d3f6860419a44b13c622b1
dennis-bilson <dennis@teksol.com.gh>
2024-06-25 12:38:34 +0000

    Added vault config for server

---
+++ vault/server.go
@@ -0,0 +1,13 @@
+package vault
+
+// GrpcServer - grpc server connection configuration
+type GrpcServer struct {
+	Host string
+	Port string
+}
+
+// HttpServer - HTTP server connection configuration
+type HttpServer struct {
+	Host string
+	Port string
+}
