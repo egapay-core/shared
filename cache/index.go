a330ac73445b31f558c234e75420da160d3739b2
dennis-bilson <dennis@teksol.com.gh>
2024-06-25 13:58:08 +0000

    added cache indexes as constants

--- cache/index.go
+++ cache/index.go
@@ -1,11 +1,11 @@
 package cache
 
 const (
-	// CustomerCacheIndex - index for customer cache (customer profile)
-	CustomerCacheIndex int = iota
-	
 	// SetupCacheIndex - index for setup cache (setup data like countries)
-	SetupCacheIndex
+	SetupCacheIndex int = iota
+	
+	// CustomerCacheIndex - index for customer cache (customer profile)
+	CustomerCacheIndex
 	
 	// CredentialsCacheIndex - index for credentials cache (credentials data like email configurations)
 	CredentialsCacheIndex
