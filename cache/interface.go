5ff1c0c6f4977106848e5af55306ee004e2ee947
dennis-bilson <dennis@teksol.com.gh>
2024-06-25 19:10:37 +0000

    added implementation for other cache functions

--- cache/interface.go
+++ cache/interface.go
@@ -16,21 +16,25 @@ var (
 type ICache interface {
 	// GetString - get a string/JSON value
 	GetString(context.Context, string) (string, error)
-
-	// GetHash - get a hash of values (e.g. a user object)
-	GetHash(context.Context, string) (map[string]interface{}, error)
-
+	
+	// GetHash - get a hash of values (e.g. a user object using `user_id` as field and `users` as key)
+	// with a specific key and field
+	GetHash(context.Context, string, string) (*string, error)
+	
+	// GetAllFromHash - get all values from a hash (e.g. a user object)
+	GetAllFromHash(context.Context, string) (map[string]interface{}, error)
+	
 	// GetList - get a list of values (e.g. a list of countries etc.)
 	GetList(context.Context, string) ([]*interface{}, error)
-
+	
 	// StoreString - store a string/JSON value
 	StoreString(context.Context, string, string, time.Duration) error
-
+	
 	// StoreHash - store a hash of values (e.g. a user object)
 	StoreHash(context.Context, string, map[string]interface{}, time.Duration) error
-
+	
 	// StoreList - store a list of values (e.g. a list of countries etc.)
 	StoreList(context.Context, string, []*interface{}, time.Duration) error
-
+	
 	Delete(context.Context, string) error
 }
