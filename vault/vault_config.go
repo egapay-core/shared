2891c265c5b00d071e03dc8de12fc7bebc29070b
dennis-bilson <dennis@teksol.com.gh>
2024-06-27 23:20:47 +0000

    added config for brokers

--- vault/vault_config.go
+++ vault/vault_config.go
@@ -3,6 +3,7 @@ package vault
 const (
 	CustomerDatabaseVaultItem = "CRDB_EGANOW_CUSTOMER_CONFIG"
 	PaymentDatabaseVaultItem  = "CRDB_EGANOW_PAYMENT_CONFIG"
-
-	CoreCacheConfigItem = "EGACORE_CACHE_CONFIG"
+	
+	CoreCacheConfigItem  = "EGACORE_CACHE_CONFIG"
+	CoreBrokerConfigItem = "EGACORE_BROKER_CONFIG"
 )
