af560a675a86534355bd04a1d5b00c60cec16e27
dennis-bilson <dennis@teksol.com.gh>
2024-06-28 15:29:28 +0000

    added consumer server interface

--- broker/server.go
+++ broker/server.go
@@ -3,7 +3,7 @@ package broker
 import "github.com/confluentinc/confluent-kafka-go/v2/kafka"
 
 // ConsumerServiceRegistrationOption is a type alias for a function that takes a pointer to a proto message
-type ConsumerServiceRegistrationOption func(*kafka.Consumer)
+type ConsumerServiceRegistrationOption func(*kafka.Consumer) error
 
 // IConsumerServer is an interface for a consumer server
 type IConsumerServer interface {
